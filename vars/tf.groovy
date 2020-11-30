0accfa9308ae3b2fbe9207f2d91b3aa538d2995e
Khabbab Saleem <khabbabs@Khabbabs-MBP.attlocal.net>
2020-11-30 14:50:52 -0600

    bash issues in step

--- vars/tf.groovy
+++ vars/tf.groovy
@@ -1,7 +1,6 @@
 void install(String version = '0.12.2' ) {
 
-    sh '''
-        #!/bin/bash 
+    sh """#!/bin/bash 
 
         echo "Installing tfswitch locally"
         wget https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh #Get the installer on to your machine
@@ -14,12 +13,12 @@ void install(String version = '0.12.2' ) {
 
         export PATH=$PATH:$CUSTOMBIN    #Add custom bin path to PATH environment
 
-        echo "setting tf version to $version "
+        echo "setting tf version to ${version} "
 
-        tfswitch -b $CUSTOMBIN/terraform $version
+        tfswitch -b $CUSTOMBIN/terraform ${version}
 
         terraform -v       
 
-    '''
+    """
     
 }
