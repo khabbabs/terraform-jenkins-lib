253e2164a51bf97367e96327ecb012ff45cf02bd
Khabbab Saleem <khabbabs@Khabbabs-MBP.attlocal.net>
2020-11-30 10:55:21 -0600

    adding tfswitcher install script

--- vars/tf.groovy
+++ vars/tf.groovy
@@ -1,5 +1,23 @@
 void install(String version = '12.0' ) {
 
-    sh "echo tf version is ${version}"
+    sh '''
+        #!/bin/bash 
+
+        echo "Installing tfswitch locally"
+        wget https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh #Get the installer on to your machine
+
+        chmod 755 install.sh            #Make installer executable
+
+        ./install.sh -b `pwd`/.bin      #Install tfswitch in a location you have permission
+
+        CUSTOMBIN=`pwd`/.bin            #set custom bin path
+
+        export PATH=$PATH:$CUSTOMBIN    #Add custom bin path to PATH environment
+
+        $CUSTOMBIN/tfswitch -b $CUSTOMBIN/terraform 0.11.7 #or simply tfswitch -b $CUSTOMBIN/terraform 0.11.7
+
+        terraform -v       
+
+    '''
     
 }
